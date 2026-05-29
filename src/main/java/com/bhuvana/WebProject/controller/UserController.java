9f77fa1dbf318045305a87a58dd8e893c6fa1ffb
BikkinaBhuvana <bikkinabhuvana16@gmail.com>
2026-05-29 23:24:48 +0530

    added crud

--- src/main/java/com/bhuvana/WebProject/controller/UserController.java
+++ src/main/java/com/bhuvana/WebProject/controller/UserController.java
@@ -2,13 +2,20 @@ package com.bhuvana.WebProject.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bhuvana.WebProject.model.User;
 import com.bhuvana.WebProject.service.UserService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,5 +30,26 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAll() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(
+            @PathVariable int id,
+            @RequestBody User u) {
+        return ResponseEntity.ok(userService.updateUser(id, u));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(
+            @RequestBody User u) {
+        User cu = userService.createUser(u);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cu);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(
+            @PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
